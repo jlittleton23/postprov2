4923c9e6f2d0012a9498e8dfc35b0bcd342c2253
jlittleton23 <jlittleton@softlayer.com>
2016-11-19 10:02:56 -0600

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,4 @@
+﻿$url = "https://dal05.objectstorage.softlayer.net/v1/AUTH_e5e0820d-faeb-480a-9901-be2a60bb149c/ashaw/setup.exe?temp_url_sig=19266ca6950a00d08a80e540690d9cadc1bce4f1&temp_url_expires=2079486989"
+$outfile = "c:\setup.exe"
+Invoke-WebRequest $url -outfile $outfile
+start-Process -Wait -FilePath $outfile -ArgumentList '/s'
