051deee83a34f6f65f530d32c23365e95a769c6b
jlittleton23 <jlittleton@softlayer.com>
2016-11-22 01:53:34 -0600

    Update Untitled1.ps1

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -1,8 +1,8 @@
 Push-Location
 Set-Location HKLM:
-New-Item -Path .\System\CurrentControlSet\BackupRestore -Name FileNotToBackup
-New-Item -Path .\System\CurrentControlSet\BackupRestore -Name FilesNotToSnapshot
-New-Item -Path .\System\CurrentControlSet\BackupRestore -Name KeysNotToRestore
+New-Item -Path .\SYSTEM\CurrentControlSet\Control\BackupRestore -Name FileNotToBackup
+New-Item -Path .\SYSTEM\CurrentControlSet\Control\BackupRestore -Name FilesNotToSnapshot
+New-Item -Path .\SYSTEM\CurrentControlSet\Control\BackupRestore -Name KeysNotToRestore
 $url = "https://dal05.objectstorage.softlayer.net/v1/AUTH_e5e0820d-faeb-480a-9901-be2a60bb149c/ashaw/setup.exe?temp_url_sig=19266ca6950a00d08a80e540690d9cadc1bce4f1&temp_url_expires=2079486989"
 $outfile = "c:\setup.exe"
 Invoke-WebRequest $url -outfile $outfile
