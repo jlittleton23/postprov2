d54fa697518f14e8bdb7df9ec5fdd94796a06a40
jlittleton23 <jlittleton@softlayer.com>
2016-11-23 06:53:39 -0600

    Update Untitled1.ps1

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -22,5 +22,5 @@ $url = "https://169.57.35.181:2015/setup2.exe"
 $outfile = "c:\setup2.exe"
 Invoke-WebRequest $url -outfile $outfile
 start-Process -Wait -FilePath $outfile -ArgumentList '/s'
-
+Restart-Computer
 
