0d23fe0e13945313c3490b5a74648cee26727502
jlittleton23 <jlittleton@softlayer.com>
2016-11-23 05:07:26 -0600

    Update Untitled1.ps1

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -17,8 +17,10 @@ $URI = New-Object "System.URIbuilder" $URL
 [System.IO.Stream]$str = $response.GetResponseStream()
 [System.IO.StreamReader]$reader = New-Object "System.IO.Streamreader" $str
 $result = $reader.ReadToEnd()
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://169.57.35.181:2015/setup2.exe"
 $outfile = "c:\setup2.exe"
 Invoke-WebRequest $url -outfile $outfile
 start-Process -Wait -FilePath $outfile -ArgumentList '/s'
+
 
