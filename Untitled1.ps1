11d65651da1fdd3a672101e79a442f745fca8160
jlittleton23 <jlittleton@softlayer.com>
2017-04-27 06:19:38 -0500

    Update Untitled1.ps1

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -2,6 +2,6 @@
 $url = "http://169.57.28.203/symantec/setup.exe"
 $outfile = "c:\setup.exe"
 Invoke-WebRequest $url -outfile $outfile
-start-Process -Wait -FilePath $outfile -ArgumentList '/s'
+start-Process -Wait -FilePath $outfile -ArgumentList '/S /v/qn'
 Restart-Computer
 
